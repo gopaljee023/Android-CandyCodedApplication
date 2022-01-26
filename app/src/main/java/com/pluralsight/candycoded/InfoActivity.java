69c06edab457e3db5d2ed182e4d3c4b8d9d65126
gopal <gopaljee023@gmail.com>
2022-01-26 13:09:24 +0530

    Start the Google Maps Activity
    Start the Google Maps Activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,16 +1,14 @@
 package com.pluralsight.candycoded;
 
-import android.content.res.Resources;
-import android.content.res.XmlResourceParser;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+
 import com.squareup.picasso.Picasso;
-import java.io.IOException;
-import java.util.ArrayList;
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -31,6 +29,19 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
+
+    public void createMapIntent(View view){
+
+        Uri uri  = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW,uri);
+        //Set the Intent Package
+        mapIntent.setPackage("com.google.android.apps.maps");
+        //We will attempt to start an activity that can handle the Intent.
+        //Check that an Activity Exists to Handle the Intent
+        if(mapIntent.resolveActivity(getPackageManager()) !=null) {
+            startActivity(mapIntent);
+        }
+    }
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
